7e91b9abfd5ae86300ec24c41a4d4b1ce2038ed4
huoxin <23447157+huoxin233@users.noreply.github.com>
2026-05-18 17:19:20 +0800

    格式化 js代码

--- js/src/common/components/WelcomeBoxWidget.js
+++ js/src/common/components/WelcomeBoxWidget.js
@@ -8,108 +8,100 @@ import SignUpModal from 'flarum/forum/components/SignUpModal';
 import Button from 'flarum/common/components/Button';
 
 export default class WelcomeBoxWidget extends Widget {
-    oninit(vnode) {
-        super.oninit(vnode);
-    }
+  oninit(vnode) {
+    super.oninit(vnode);
+  }
 
-    className() {
-        return 'welcomebox-widget';
-    }
+  className() {
+    return 'welcomebox-widget';
+  }
 
-    icon() {
-        return 'fas fa-user-tie';
-    }
+  icon() {
+    return 'fas fa-user-tie';
+  }
 
-    content() {
-        const user = app.session.user;
-        const money = user.attribute('money'); // 获取金钱数量
-        const moneyName = app.forum.attribute('antoinefr-money.moneyname').replace('[money] ', ''); // 获取金钱名称
-        const useWidget = app.forum.attribute('justoverclock-welcomebox.UseWidget');
-        const isGuestBoxHidden = app.forum.attribute('HideGuestBox');
-        const SettingsLink = app.route('settings');
-        const ImgAvatar =
-            app.forum.attribute('imgUrl') || app.forum.attribute('baseUrl') + '/assets/extensions/justoverclock-welcomebox/no-avatar.png';
+  content() {
+    const user = app.session.user;
+    const money = user.attribute('money'); // 获取金钱数量
+    const moneyName = app.forum.attribute('antoinefr-money.moneyname').replace('[money] ', ''); // 获取金钱名称
+    const useWidget = app.forum.attribute('justoverclock-welcomebox.UseWidget');
+    const isGuestBoxHidden = app.forum.attribute('HideGuestBox');
+    const SettingsLink = app.route('settings');
+    const ImgAvatar = app.forum.attribute('imgUrl') || app.forum.attribute('baseUrl') + '/assets/extensions/justoverclock-welcomebox/no-avatar.png';
 
-        if (useWidget && user) {
-            return (
-                <div className="containerwb">
-                    <div className="backgrwb">
-                        {Button.component({
-                            icon: 'fas fa-sign-out-alt logoutt',
-                            title: app.translator.trans('core.forum.header.log_out_button'),
-                            className: 'Dropdown-toggle Button logwbox',
-                            onclick: app.session.logout.bind(app.session),
-                        })}
-                        <div>
-                            <a href={app.route.user(user)}>
-                                <div className="avatarwb">{AvatarEditor.component({ user })}</div>
-                            </a>
-                            <div className="contentwb">
-                                <div className="textinfo">
-                                    {app.translator.trans('flarum-ext-welcomebox.forum.wback')}
-                                    <br />
-                                    <strong>{username(user)}</strong>
-                                </div>
-                                <div className="cont">
-                                    <div className="circletop">
-                                        <a href={SettingsLink} title={app.translator.trans('core.forum.settings.title')}>
-                                            <i className="menuicon fas fa-tasks" />
-                                        </a>
-                                        <a href={app.route.user(user)} title={app.translator.trans('flarum-ext-welcomebox.forum.tooltipProfile')}>
-                                            <i className="menuicon far fa-user" />
-                                        </a>
-                                        <a
-                                            href={app.route.user(user) + '/mentions'}
-                                            title={app.translator.trans('flarum-ext-welcomebox.forum.tooltipMentions')}
-                                        >
-                                            <i className="menuicon fas fa-at" />
-                                        </a>
-                                        <a
-                                            href={app.route.user(user) + '/discussions'}
-                                            title={app.translator.trans('flarum-ext-welcomebox.forum.tooltipDisclist')}
-                                        >
-                                            <i className="menuicon far fa-list-alt" />
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="iconbadge">{listItems(user.badges().toArray())}</div>
-                            <div className="ulwb contentwb">
-                                <li>
-                                    <label className="textinfo">{app.translator.trans('flarum-ext-welcomebox.forum.npost')}</label>:{' '}
-                                    <strong className="textinfo">{formatNumber(user.commentCount())}</strong>
-                                </li>
-                                <li>
-                                    <label className="textinfo">{app.translator.trans('flarum-ext-welcomebox.forum.discussion')}</label>:{' '}
-                                    <strong className="textinfo">{formatNumber(user.discussionCount())}</strong>
-                                </li>
-                                <li>
-                                    <label className="textinfo">{moneyName}</label>:{' '}
-                                    <strong className="textinfo">{money}</strong>
-                                </li>
-                            </div>
-                        </div>
-                    </div>
+    if (useWidget && user) {
+      return (
+        <div className="containerwb">
+          <div className="backgrwb">
+            {Button.component({
+              icon: 'fas fa-sign-out-alt logoutt',
+              title: app.translator.trans('core.forum.header.log_out_button'),
+              className: 'Dropdown-toggle Button logwbox',
+              onclick: app.session.logout.bind(app.session),
+            })}
+            <div>
+              <a href={app.route.user(user)}>
+                <div className="avatarwb">{AvatarEditor.component({ user })}</div>
+              </a>
+              <div className="contentwb">
+                <div className="textinfo">
+                  {app.translator.trans('flarum-ext-welcomebox.forum.wback')}
+                  <br />
+                  <strong>{username(user)}</strong>
                 </div>
-            );
-        } else if (!user && isGuestBoxHidden && useWidget) {
-            return (
-                <div className="containerwb">
-                    <div className="backgrwbguest">
-                        <img className="guestavatarimg" src={ImgAvatar} />
-                        <div className="guesttext">{app.translator.trans('flarum-ext-welcomebox.forum.welcomeguest')}</div>
-                        <p className="guestdesc">{app.translator.trans('flarum-ext-welcomebox.forum.notregistered')}</p>
-                        <button
-                            className=".SplitDropdown-button Button Button--primary hasIcon"
-                            type="button"
-                            onclick={(componentClass, attrs) => app.modal.show(SignUpModal, attrs)}
-                        >
-                            {app.translator.trans('core.forum.header.sign_up_link')}
-                        </button>
-                        <div className="contentwb" />
-                    </div>
+                <div className="cont">
+                  <div className="circletop">
+                    <a href={SettingsLink} title={app.translator.trans('core.forum.settings.title')}>
+                      <i className="menuicon fas fa-tasks" />
+                    </a>
+                    <a href={app.route.user(user)} title={app.translator.trans('flarum-ext-welcomebox.forum.tooltipProfile')}>
+                      <i className="menuicon far fa-user" />
+                    </a>
+                    <a href={app.route.user(user) + '/mentions'} title={app.translator.trans('flarum-ext-welcomebox.forum.tooltipMentions')}>
+                      <i className="menuicon fas fa-at" />
+                    </a>
+                    <a href={app.route.user(user) + '/discussions'} title={app.translator.trans('flarum-ext-welcomebox.forum.tooltipDisclist')}>
+                      <i className="menuicon far fa-list-alt" />
+                    </a>
+                  </div>
                 </div>
-            );
-        }
+              </div>
+              <div className="iconbadge">{listItems(user.badges().toArray())}</div>
+              <div className="ulwb contentwb">
+                <li>
+                  <label className="textinfo">{app.translator.trans('flarum-ext-welcomebox.forum.npost')}</label>:{' '}
+                  <strong className="textinfo">{formatNumber(user.commentCount())}</strong>
+                </li>
+                <li>
+                  <label className="textinfo">{app.translator.trans('flarum-ext-welcomebox.forum.discussion')}</label>:{' '}
+                  <strong className="textinfo">{formatNumber(user.discussionCount())}</strong>
+                </li>
+                <li>
+                  <label className="textinfo">{moneyName}</label>: <strong className="textinfo">{money}</strong>
+                </li>
+              </div>
+            </div>
+          </div>
+        </div>
+      );
+    } else if (!user && isGuestBoxHidden && useWidget) {
+      return (
+        <div className="containerwb">
+          <div className="backgrwbguest">
+            <img className="guestavatarimg" src={ImgAvatar} />
+            <div className="guesttext">{app.translator.trans('flarum-ext-welcomebox.forum.welcomeguest')}</div>
+            <p className="guestdesc">{app.translator.trans('flarum-ext-welcomebox.forum.notregistered')}</p>
+            <button
+              className=".SplitDropdown-button Button Button--primary hasIcon"
+              type="button"
+              onclick={(componentClass, attrs) => app.modal.show(SignUpModal, attrs)}
+            >
+              {app.translator.trans('core.forum.header.sign_up_link')}
+            </button>
+            <div className="contentwb" />
+          </div>
+        </div>
+      );
     }
+  }
 }
